91abfb7107522719e798080923390ebccd12d4b0
b01502102 <b01502102.ntu.edu.tw>
2015-10-09 17:21:40 +0800

    update

--- neural_network.py
+++ neural_network.py
@@ -2,19 +2,20 @@ import random
 import theano
 import theano.tensor as T
 class init_weight:
-	weights = [[]]
+    weights = [[]]
     def __init__(self,prev_num,cur_num):
         self.prev_num = prev_num
         self.cur_num = cur_num
-    def random(self)
-            weights = [[random.random() for x in range(prev_num)] for x in range(cur_num)] 
+    def random(self):
+        weights = [[random.random() for x in range(self.prev_num)] for x in range(self.cur_num)] 
 if __name__ == '__main__':
-    inputs = []
-    layers_num = input("please input the number of layers")
+    inputs = [0.1,0.5,0.32,0.45,0.67,0.91,0.33]
+    layers_num = input("please input the number of layers:")
     layers_num = int(layers_num)
-    neuron_num = []
-    for i in range (layers_num)
-        x = input("please input the number of neuron in layer"+i)
+    neuron_num = [len(inputs)]
+
+    for i in range (layers_num ):
+        x = input("please input the number of neuron in layer:")
         x = int(x)
         neuron_num.append(x)
     a = T.matrix()
@@ -22,7 +23,7 @@ if __name__ == '__main__':
     c = a * b
     multiply = theano.function([a,b],c)    
     ni = inputs
-    for i in range (layer_num - 1)
+    for i in range (layers_num - 1):
         w = init_weight(neuron_num[i+1],neuron_num[i])
         w.random()
         ni = multiply(w.weights,ni)
