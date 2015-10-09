15b959c95f869095f0e62f6b3c50d8c06606e149
b01502102 <b01502102.ntu.edu.tw>
2015-10-09 18:22:26 +0800

    update

--- neural_network.py
+++ neural_network.py
@@ -2,14 +2,13 @@ import random
 import theano
 import theano.tensor as T
 class init_weight:
-    weights = [[]]
     def __init__(self,prev_num,cur_num):
         self.prev_num = prev_num
         self.cur_num = cur_num
     def random(self):
-        weights = [[random.random() for x in range(self.prev_num)] for x in range(self.cur_num)] 
+        self.weights = [[random.random() for x in range(self.prev_num)] for x in range(self.cur_num)]
 if __name__ == '__main__':
-    inputs = [0.1,0.5,0.32,0.45,0.67,0.91,0.33]
+    inputs = [[0.1],[0.5],[0.32],[0.45],[0.67],[0.91],[0.33]]
     layers_num = input("please input the number of layers:")
     layers_num = int(layers_num)
     neuron_num = [len(inputs)]
@@ -20,11 +19,11 @@ if __name__ == '__main__':
         neuron_num.append(x)
     a = T.matrix()
     b = T.matrix()
-    c = a * b
+    c = T.dot(a,b)
     multiply = theano.function([a,b],c)    
     ni = inputs
     for i in range (layers_num - 1):
-        w = init_weight(neuron_num[i+1],neuron_num[i])
+        w = init_weight(neuron_num[i],neuron_num[i+1])
         w.random()
         ni = multiply(w.weights,ni)
     print ni
