7795a7a035afac339314349739d5400fe8079a34
patrickchen <patrickchen1994@gmail.com>
2015-10-12 21:23:44 +0800

    ModelFactory version 5 * Seemingly finish debugging, waiting for bigger amount of data

--- ModelFactory.py
+++ ModelFactory.py
@@ -1,7 +1,6 @@
 import theano.tensor as tensor
 from theano import shared, function, grad
 import numpy as np
-__author__ = 'patrickchen'
 
 
 class ModelFactory:
@@ -15,8 +14,8 @@ class ModelFactory:
         self.y_evaluated = None
         self.cost = None
         self.update = None
-        self.x_input = tensor.fmatrix()
-        self.y_input = tensor.fmatrix()
+        self.x_input = tensor.fmatrix("X_input")
+        self.y_input = tensor.fmatrix("Y_input")
         self.W_array = []
         self.B_array = []
 
@@ -35,35 +34,31 @@ class ModelFactory:
             len(self.layer_neuron_num_list)
         )
         temp = [self.input_dim] + self.layer_neuron_num_list + [self.output_dim]
-        for i in range(0, len(temp) - 1):
+        for i in range(len(temp) - 1):
             wp = np.zeros((temp[i], temp[i + 1]), dtype='float32')
             bp = np.zeros((1, temp[i + 1]), dtype='float32')
-            # TODO: W are set to zeros
-            self.W_array.append(shared(wp, borrow=True))
-            self.B_array.append(shared(bp, borrow=True))
+
+            # TODO: W and b are set to zeros
+            self.W_array.append(shared(wp, name="W%d" % i, borrow=True))
+            self.B_array.append(shared(bp, name="B%d" % i, borrow=True))
 
     def _create_model(self):
         result = self.x_input
-        for i in range(len(self.W_array) - 1):
+        for i in range(len(self.W_array)):
             result = ModelFactory._layer_propagate(result, self.W_array[i], self.B_array[i])
         self.y_evaluated = result
 
     def _define_update_function(self):
         self.cost = ModelFactory._cost_function(self.y_evaluated, self.y_input)
         g = grad(self.cost, self.W_array + self.B_array)
-        update_pairs = []
-        for i in range(len(self.W_array)):
-            update_pairs.append((self.W_array[i], self.W_array[i] - self.learning_rate * g[i]))
-        self.update = function([self.x_input, self.y_input], g, updates=update_pairs)
 
-    @staticmethod
-    def _create_matrix_list(row, col):
-        re = []
-        for i in range(0, row):
-            re.append([])
-            for j in range(0, col):
-                re[i].append(0)  # TODO: Use random number generator to get initial value
-        return re
+        update_pairs = []
+        j = len(self.W_array)
+        for i in range(len(self.B_array)):
+            update_pairs.append((self.W_array[i], self.W_array[i] - self.learning_rate * g[i]))
+            update_pairs.append((self.B_array[i], self.B_array[i] - self.learning_rate * g[i + j]))
+
+        self.update = function([self.x_input, self.y_input], g, updates=update_pairs)
 
     @staticmethod
     def _act_function(x):
@@ -77,16 +72,11 @@ class ModelFactory:
     def _cost_function(func, out):
         return (func - out).norm(1)
 
-    def exec_one(self, x_input, y_input):
+    def train_one(self, x_input, y_input):
         if len(x_input) != self.batch_num:
             print "Error: Input batch size not equals to the number pre-defined."
             exit(1)
         if len(x_input[0]) != self.input_dim or len(y_input[0]) != self.output_dim:
             print "Error: Input/Output dimension not equals to the number pre-defined."
             exit(1)
-        for i in range(len(x_input)):
-            x_input[i] = x_input[i] + [1]
-        for i in range(len(y_input)):
-            y_input[i] = y_input[i] + [1]
-        self.update(x_input, y_input)
-        pass
+        return self.update(x_input, y_input)
