a1bd6ef4634c8829d004fef58a57554ac9922556
patrickchen <patrickchen1994@gmail.com>
2015-10-20 22:22:33 +0800

    ModelFactory version 9.1 * Debug

--- testbench.py
+++ testbench.py
@@ -1,7 +1,7 @@
 import cPickle
-from random import randrange
+import random
+from random import randrange, randint
 from sys import stdout
-
 from math import floor
 
 from ModelFactory import *
@@ -28,7 +28,7 @@ def my_print(title, content=None, switch=True):
     if content is None:
         print BColors.OKGREEN, "*", title, ":", BColors.ENDC
         return
-    print BColors.OKGREEN, "*", title, ":", BColors.ENDC, "\n", content
+    print BColors.OKGREEN, "*", title, ":", BColors.ENDC, content
 
 
 class TestBench:
@@ -38,10 +38,11 @@ class TestBench:
         self.train_input_data = []
         self.answer_map_file = "answer_map_sub.txt"
         self.train_answer_data = []
+        self.train_segment = 400
 
         # Define test file
         self.test_data = []
-        self.test_input_file = "test.ark"
+        self.test_input_file = "test_sub.ark"
         self.test_output_file = "test_ans.csv"
         self.W_parm = None
         self.B_parm = None
@@ -74,6 +75,8 @@ class TestBench:
 
         self.prompt = "Enter a command:"
         while True:
+            self.prompt = "Enter a command:" if self.modified is False else "Enter a command[Modified]:"
+
             command = raw_input(BColors.HEADER + self.prompt + BColors.ENDC)
             self.__exec_command__(command)
 
@@ -88,13 +91,13 @@ class TestBench:
 
     def __exec_command__(self, cmd):
         dispatcher = {
-            "status": self._get_status,
-            "train": self._train_data,
-            "load": self._load_parameter,
-            "run": self._run,
-            "output": self._output_result,
-            "set": self._set,
-            "quit": self._quit
+            "status": self.status,
+            "train": self.train,
+            "load": self.load,
+            "run": self.run,
+            "output": self.output,
+            "set": self.set,
+            "quit": self.quit
         }
         commands = cmd.strip().split(';')
 
@@ -105,16 +108,17 @@ class TestBench:
             func = dispatcher.get(m[0], lambda (xx): self.__do_nothing__)
             func(m)
 
-    '''
-    #################### Dispatcher Functions ##############################
-    '''
-
     def __do_nothing__(self, x):
+        print BColors.WARNING, "Command not found.", BColors.ENDC
         pass
 
-    def _get_status(self, param):
+    '''
+    #################### Status Functions #####################################
+    '''
+
+    def status(self, param):
         if self.model is not None:
-            my_print("Model created")
+            my_print("Model have been created")
             if self.modified:
                 my_print("Correct ratio", self)
                 pass
@@ -124,13 +128,16 @@ class TestBench:
             # my_print("Dropout enable", "True" if self.dropout_enable else "False")
             pass
         else:
-            my_print("Model haven't created")
+            print BColors.OKGREEN, "Model haven't been created", BColors.ENDC
             my_print("Layer", self.layer)
             my_print("Batch number", self.batch_number)
             my_print("Input dimension", self.input_dimension)
             my_print("Output dimension", self.output_dimension)
             my_print("Learning rate", self.lr)
 
+    '''
+    #################### Training Functions ###################################
+    '''
     @staticmethod
     def update_display(cur, total, cur_epoch, cost, acc):
         stdout.write(
@@ -141,59 +148,136 @@ class TestBench:
         )
         pass
 
-    def _train_data(self, param):
+    def train(self, param):
+        epoch = 1
+        if len(param) > 1 and param[1].isdigit():
+            epoch = int(param[1])
+        self.train_data(epoch)
+
+    def train_data(self, epoch):
+        train_times = self.train_segment * epoch
         if len(self.train_input_data) == 0:
-            self._load_train_data()
+            self.load_train_input_data()
         if self.model is None:
             self.create_model()
 
-        epoch = 1
-        train_segment = 400
-        train_segment = int(floor(train_segment / self.batch_number) * self.batch_number)
-        train_time = train_segment
-        if len(param) > 1 and param[1].isdigit():
-            train_time *= int(param[1])
         train_batch_x = range(self.batch_number)
         train_batch_y = range(self.batch_number)
         i = 0
         m = 0
-        while i < train_time:
+        cost = 0
+        acc = 0
+        while i < train_times:
             for j in range(self.batch_number):
-                num = random.randint(0, train_segment)
-
-                train_batch_x[j], train_batch_y[j] = self.get_one_data(num)
+                num = random.randrange(0, self.train_segment)
+                train_batch_x[j], train_batch_y[j] = self.get_one_data(num), self.get_one_answer(num)
 
             self.model.train_one(train_batch_x, train_batch_y)
+
             i += self.batch_number
-            m += self.batch_number
-            if m > train_segment:
-                # one epoch
-                epoch += 1
-                m = 0
-            cost = self.model.cost_function(train_batch_x, train_batch_y)
-            acc = self._test(train_segment)
+            if m > self.train_segment:
+                cost = self.model.cost_function(train_batch_x, train_batch_y)
+                acc = self._test(self.train_segment)
+                m -= self.train_segment
+
             TestBench.update_display(cur=i,
-                                     total=train_time,
-                                     cur_epoch=epoch,
+                                     total=train_times,
+                                     cur_epoch=i / self.train_segment,
                                      cost=cost,
                                      acc=acc
                                      )
         stdout.write("\n")
 
+    '''
+    #################### Load Functions #######################################
+    '''
+
+    def load(self, param):
+        id = 0
+        if len(param) > 1 and param[1].isdigit():
+            id = int(param[1])
+        self.load_parameter(id)
+
+    def load_parameter(self, id):
+        filename_w = "parameter_W_%s.txt" % id
+        filename_b = "parameter_B_%s.txt" % id
+        filename_i = "parameter_I_%s.txt" % id
+        my_print("Load parameters from parameter_X_%s.txt" % id)
+
+        # TODO: Keep parameters of two test consistent
+
+        try:
+            i_parm_data = open(filename_i, 'r')
+            # self.update_parameter(i_parm_data)
+            if self.model is None:
+                self.create_model()
+
+            w_parm_data = file(filename_w, 'rb')
+            b_parm_data = file(filename_b, 'rb')
+            w_parm = cPickle.load(w_parm_data)
+            b_parm = cPickle.load(b_parm_data)
+            self.model.load_parm(w_parm, b_parm)
+        except IOError:
+            my_print(BColors.FAIL + "File not found. Do nothing." + BColors.ENDC)
+            return
+
     def get_one_data(self, num):
-        type_index = self.ans_type.index(str(self.train_answer_data[num][1].split('\n')[0]))
-        y = [0] * 48
-        y[type_index] = 1
-        return self.train_input_data[num][1:70], y
+        return self.train_input_data[num][1:self.input_dimension + 1]
+
+    def get_one_answer(self, num):
+        type_index = self.ans_type.index(str(self.train_answer_data[num][1].strip()))
+        g = [0] * self.output_dimension
+        g[type_index] = 1
+        return g
+
+    def run(self, param):
         pass
 
-    def _run(self, param):
+    '''
+    #################### Output Functions #####################################
+    '''
+
+    def output(self, param):
+        output_dispatcher = {
+            "progress": self._output_progress,
+            "csv": self._output_csv
+        }
+        if len(param) > 1:
+            output_command = output_dispatcher.get(param[1], lambda (xx): self.__do_nothing__)
+            output_command(param)
+            return
+        else:
+            print BColors.WARNING + "Output parameter without any argument. Do nothing." + BColors.ENDC
+            return
         pass
 
-    def _output_result(self, param):
+    def _output_progress(self, param):
         pass
 
-    def _set(self, param):
+    def _output_csv(self, param):
+        _id = 0
+        if len(param) > 2 and param[2].isdigit():
+            _id = int(param[2])
+
+        if len(self.test_data) == 0:
+            self.load_test_data()
+
+        filename_test = "test_ans_%d.txt" % _id
+        test_stream = open(filename_test, 'w')
+        my_print("Writing test answer data to %s" % filename_test)
+
+        test_c = MAP()
+
+        test_stream.write('Id,Prediction\n')
+
+        for i in range(len(self.test_data)):
+            x, y = [self.get_one_data(i)], [[0] * self.output_dimension]
+            ya = self.model.y_evaluated_function(x, y)
+            value = "%s,%s" % (self.test_data[i][0], test_c.map(ya))
+            test_stream.write(value)
+            test_stream.write('\n')
+
+    def set(self, param):
         set_dispatcher = {
             "layer": self._set_layer,
             "batch": self._set_batch
@@ -247,32 +331,15 @@ class TestBench:
             self.batch_number = d
             my_print("Batch number is set to %d" % self.batch_number)
 
-    def _load_parameter(self, param):
-        filename_w = None
-        filename_b = None
-        if param[1] == "id":
-            filename_w = "parameter_W_%s.txt" % param[2]
-            filename_b = "parameter_B_%s.txt" % param[2]
-            filename_i = "parameter_I_%s.txt" % param[2]
-        my_print("Load parameters from parameter_X_%s.txt" % param[2])
+    def save_parameter(self, id):
+        f = file('parameter_W_%s.txt' % id, 'wb')
+        cPickle.dump(self.model.W, f, protocol=cPickle.HIGHEST_PROTOCOL)
+        f.close()
+        f = file('parameter_B_%s.txt' % id, 'wb')
+        cPickle.dump(self.model.B, f, protocol=cPickle.HIGHEST_PROTOCOL)
+        f.close()
 
-        # TODO: Keep parameters of two test consistent
-
-        try:
-            if self.model is None:
-                i_parm_data = file(filename_i, 'rb')
-                self.create_model()
-
-            w_parm_data = file(filename_w, 'rb')
-            b_parm_data = file(filename_b, 'rb')
-            w_parm = cPickle.load(w_parm_data)
-            b_parm = cPickle.load(b_parm_data)
-            self.model.load_parm(w_parm, b_parm)
-        except IOError:
-            my_print(BColors.FAIL + "File not found. Do nothing." + BColors.ENDC)
-            return
-
-    def _quit(self, param):
+    def quit(self, param):
         if self.modified is True:
             if len(param) > 1 and param[1] == "force":
                 exit()
@@ -284,7 +351,7 @@ class TestBench:
                     return
         exit()
 
-    def _load_train_data(self):
+    def load_train_input_data(self):
         my_print("Load training input data from %s" % self.train_file)
         for line in open(self.train_file, 'r'):
             input_x = line.split()
@@ -296,22 +363,24 @@ class TestBench:
             ans_x = line.split(',')
             self.train_answer_data.append(ans_x)
 
-    def _load_test_data(self):
+    def load_test_data(self):
         for line in open(self.test_input_file, 'r'):
             test_x = line.split()
             test_x = [TestBench.float_convert(x) for x in test_x]
             self.test_data.append(test_x)
 
-    def _output_test_file(self):
+    def save_test_data(self):
+        if len(self.test_input_file) == 0:
+            self.load_test_data()
         my_print("Writing test answer data to %s", self.answer_map_file)
         test_stream = open(self.test_output_file, 'w')
         test_c = MAP()
-        y = None
+        y = [0] * self.output_dimension
         test_stream.write('Id,Prediction\n')
         self.model.load_parm(self.W_parm, self.B_parm)
 
         for i in range(len(self.test_data)):
-            ya = self.model.y_evaluated_function([self.test_data[i][1:self.input_dimension]], y)
+            ya = self.model.y_evaluated_function([self.test_data[i][1:self.input_dimension]], [y])
             value = str(
                 (self.test_data[i][0], test_c.map(ya, ))
             )
@@ -320,7 +389,7 @@ class TestBench:
         # print test.W_array[0].get_value()
         return
 
-    def _output_training_progress(self):
+    def save_training_progress(self):
         pass
 
     def _test(self, training_segment):
@@ -341,54 +410,48 @@ class TestBench:
                 # print [str(ans[m][1].split('\n')[0])]
         return 1.0 - err / float(_1)
 
-    def run(self, batch):
-        training_segment = 1000000
+    # def __run(self, batch):
+    #     training_segment = 1000000
+    #
+    #     batch_number = batch * 1000
+    #     X = None
+    #     Y = None
+    #     i = 0
+    #     acc = 0.0
+    #     W_new = []
+    #     B_new = []
+    #     c = MAP()
+    #     while True:
+    #         X = []
+    #         yy = []
+    #         for k in range(batch_number):
+    #             num = randrange(0, training_segment)
+    #             if i >= 1000000:  # i >= 1124823:
+    #                 i = 0
+    #                 err = 0.0
+    #                 for m in range(124823):
+    #                     Ya = self.model.y_evaluated_function([self.train[1000000 + m][1:70]], Y)[0]
+    #                     if [c.map(Ya)] != [str(self.ans[1000000 + m][1].split('\n')[0])]:
+    #                         err += 1
+    #                         # print [c.map(Ya)]
+    #                         # print [str(ans[m][1].split('\n')[0])]
+    #                 acc = 1.0 - err / 124823.0
+    #                 # print err
+    #                 print acc
+    #             typeidx = self.anstype.index(str(self.ans[num][1].split('\n')[0]))
+    #             y = [0] * 48
+    #             y[typeidx] = 1
+    #             yy.append(y)
+    #             X.append(self.train[num][1:70])
+    #             i += 1
+    #         Y = yy
+    #         if i % 10000 == 0:
+    #             print i
+    #             # print test.y_evaluated_function(X,Y)
+    #             # print [test.W_array[0].get_value(),test.W_array[1].get_value()]
+    #         self.model.train_one(X, Y)
 
-        batch_number = batch * 1000
-        X = None
-        Y = None
-        i = 0
-        acc = 0.0
-        W_new = []
-        B_new = []
-        c = MAP()
-        while True:
-            X = []
-            yy = []
-            for k in range(batch_number):
-                num = randrange(0, training_segment)
-                if i >= 1000000:  # i >= 1124823:
-                    i = 0
-                    err = 0.0
-                    for m in range(124823):
-                        Ya = self.model.y_evaluated_function([self.train[1000000 + m][1:70]], Y)[0]
-                        if [c.map(Ya)] != [str(self.ans[1000000 + m][1].split('\n')[0])]:
-                            err += 1
-                            # print [c.map(Ya)]
-                            # print [str(ans[m][1].split('\n')[0])]
-                    acc = 1.0 - err / 124823.0
-                    # print err
-                    print acc
-                typeidx = self.anstype.index(str(self.ans[num][1].split('\n')[0]))
-                y = [0] * 48
-                y[typeidx] = 1
-                yy.append(y)
-                X.append(self.train[num][1:70])
-                i += 1
-            Y = yy
-            if i % 10000 == 0:
-                print i
-                # print test.y_evaluated_function(X,Y)
-                # print [test.W_array[0].get_value(),test.W_array[1].get_value()]
-            self.model.train_one(X, Y)
 
-    def save_parameter(self):
-        f = file('parameter_W.txt', 'wb')
-        cPickle.dump(self.model.W, f, protocol=cPickle.HIGHEST_PROTOCOL)
-        f.close()
-        f = file('parameter_B.txt', 'wb')
-        cPickle.dump(self.model.B, f, protocol=cPickle.HIGHEST_PROTOCOL)
-        f.close()
 
     @staticmethod
     def get_correctness_ratio(correct, total):
